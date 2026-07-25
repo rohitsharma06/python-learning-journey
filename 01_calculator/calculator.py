2b80746c5106806d7b2b4c67778eade2a3ec1fea
Rohit <rohit2000sharma06@gmail.com>
2026-07-25 15:46:12 +0530

    complete calculator version 3

--- 01_calculator/calculator.py
+++ 01_calculator/calculator.py
@@ -1,26 +1,42 @@
-print("Simple calculator")
 
-print("1.Addition")
-print("2.Substraction")
-print("3.Multiplication")
-print("4.Division")
+while True:
+    print("Simple calculator")
 
-choice = int(input("Enter your Choice:- "))
+    print("1.Addition")
+    print("2.Substraction")
+    print("3.Multiplication")
+    print("4.Division")
+    print("5.Exit")
 
-first_number = int(input("Enter your First Number:- "))
-second_number = int(input("Enter your Second Number:- "))
+    choice = int(input("Enter your Choice:- "))
+    if choice == 5:
+        print("Thank You")
+        break
 
-if choice == 1:
-    result = first_number + second_number
-    print("Result =", result)
-elif choice == 2:
-    result = first_number - second_number
-    print("Result =", result)
-elif choice == 3:
-    result = first_number * second_number
-    print("Result =", result)
-elif choice == 4:
-    result = first_number / second_number
-    print("Result =", result)
-else:
-    print("Invalid Choice!")
+
+
+    if choice == 1:
+        first_number = int(input("Enter your First Number:- "))
+        second_number = int(input("Enter your Second Number:- "))
+        result = first_number + second_number
+        print("Result =", result)
+    elif choice == 2:
+        first_number = int(input("Enter your First Number:- "))
+        second_number = int(input("Enter your Second Number:- "))
+        result = first_number - second_number
+        print("Result =", result)
+    elif choice == 3:
+        first_number = int(input("Enter your First Number:- "))
+        second_number = int(input("Enter your Second Number:- "))
+        result = first_number * second_number
+        print("Result =", result)
+    elif choice == 4:
+        first_number = int(input("Enter your First Number:- "))
+        second_number = int(input("Enter your Second Number:- "))
+        if second_number == 0:
+            print("Cannot Divide By zero")
+        else:
+            result = first_number / second_number
+            print("Result =", result)
+    else:
+        print("Invalid Choice!")
