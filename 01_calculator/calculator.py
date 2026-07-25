86e823769794e383850a9b148ea4c0486fc4c50b
Rohit <rohit2000sharma06@gmail.com>
2026-07-25 16:36:55 +0530

    Refactor calculator input handling

--- 01_calculator/calculator.py
+++ 01_calculator/calculator.py
@@ -13,26 +13,21 @@ while True:
         print("Thank You")
         break
 
+    if choice >= 1 and choice <= 4:
+        first_number = int(input("Enter your First Number:- "))
+        second_number = int(input("Enter your Second Number:- "))
 
 
     if choice == 1:
-        first_number = int(input("Enter your First Number:- "))
-        second_number = int(input("Enter your Second Number:- "))
         result = first_number + second_number
         print("Result =", result)
     elif choice == 2:
-        first_number = int(input("Enter your First Number:- "))
-        second_number = int(input("Enter your Second Number:- "))
         result = first_number - second_number
         print("Result =", result)
     elif choice == 3:
-        first_number = int(input("Enter your First Number:- "))
-        second_number = int(input("Enter your Second Number:- "))
         result = first_number * second_number
         print("Result =", result)
     elif choice == 4:
-        first_number = int(input("Enter your First Number:- "))
-        second_number = int(input("Enter your Second Number:- "))
         if second_number == 0:
             print("Cannot Divide By zero")
         else:
