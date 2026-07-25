eb6a1dfa3b6aa59a0f7934af03cd7e62dc46caa8
Rohit <rohit@gmail.com>
2026-07-25 11:59:40 +0530

    Create Calculator version 1 with addition

--- 01_calculator/calculator.py
+++ 01_calculator/calculator.py
@@ -1,1 +1,6 @@
 print("Simple calculator")
+first_number = int(input("Enter your First Number:- "))
+second_number = int(input("Enter your Second Number:- "))
+
+result = first_number + second_number
+print("Result =",result)
