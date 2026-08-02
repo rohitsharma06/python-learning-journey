056c57867029943725a53cdd7cbfe19a51cf292f
Rohit <rohit2000sharma06@gmail.com>
2026-08-02 16:23:14 +0530

    version 5 completed and updated

--- 03_number_guessing_game/README.md
+++ 03_number_guessing_game/README.md
@@ -18,3 +18,50 @@
 - break
 - random.randint()
 - Comparison Operators
+
+## Version 2
+
+### New Features
+- Added attempts counter
+- Displays total attempts after guessing correctly
+
+### New Concepts Learned
+- Counter Variable
+- += Operator
+- f-string
+
+## Version 3
+
+### New Features
+- Added maximum attempts
+- Game Over message
+- Displays attempts left after every guess
+
+### New Concepts Learned
+- Game Logic
+- Limiting Attempts
+- break Statement
+
+## Version 4
+
+### New Features
+- Added difficulty selection
+- Easy Mode (10 Attempts)
+- Medium Mode (7 Attempts)
+- Hard Mode (5 Attempts)
+
+### New Concepts Learned
+- Multiple Conditions
+- Difficulty Selection
+- Dynamic Variables
+
+## Version 5
+
+### New Features
+- Added Play Again option
+- Multiple games can be played without restarting the program
+
+### New Concepts Learned
+- Nested while Loops
+- Outer Loop
+- Inner Loop
--- 03_number_guessing_game/guess.py
+++ 03_number_guessing_game/guess.py
@@ -1,21 +1,49 @@
 import random
 
-secret_number = random.randint(1,10)
-
-attempt = 0
-
 while True:
-    guess = int(input("Guess a number between 1 to 10:- "))
 
-    attempt += 1
+    secret_number = random.randint(1,10)
 
-    if guess == secret_number:
-        print("Congratulations! You guessed correctly!")
-        print(f"You guessed The number in {attempt} attempts.")
-        break
+    print("Select Difficulty:")
+    print("1.Easy \n2.Medium \n3.Hard")
 
-    elif guess > secret_number:
-        print("Your guess was too high!")
+    difficulty = int(input("Enter your choice: "))
 
+    attempts = 0
+
+    if difficulty == 1:
+        max_attempt =10
+    elif difficulty == 2:
+        max_attempt = 7
     else:
-        print("Your guess is too low!")
+        max_attempt = 5
+
+    while True:
+        guess = int(input("Guess a number between 1 to 10:- "))
+
+        attempt += 1
+        print(f"Attempt Left: {max_attempt - attempts}")
+        if guess == secret_number:
+            print("Congratulations! You guessed correctly!")
+            print(f"You guessed The number in {attempts} attempts.")
+            break
+
+        elif guess > secret_number:
+            print("Your guess was too high!")
+            if attempts == max_attempt:
+                print("Game Over")
+                print(f"The correct number was: {secret_number}")
+                break
+
+        else:
+            print("Your guess is too low!")
+            if attempts == max_attempt:
+                print("Game Over")
+                print(f"The correct number was: {secret_number}")
+                break
+
+    play_again = input("Do you want to play again? (y/n): ")
+
+    if play_again == "n":
+        print("Thank you for playing!")
+        break
