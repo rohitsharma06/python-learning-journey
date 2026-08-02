9a1780e8fac595891e7fe29a3d2d343180b22063
Rohit <rohit2000sharma06@gmail.com>
2026-08-02 15:53:32 +0530

    Version 2 completed

--- 03_number_guessing_game/guess.py
+++ 03_number_guessing_game/guess.py
@@ -1,11 +1,17 @@
 import random
 
 secret_number = random.randint(1,10)
+
+attempt = 0
+
 while True:
     guess = int(input("Guess a number between 1 to 10:- "))
 
+    attempt += 1
+
     if guess == secret_number:
         print("Congratulations! You guessed correctly!")
+        print(f"You guessed The number in {attempt} attempts.")
         break
 
     elif guess > secret_number:
