0295819d182ae246c6377172f3674ea6a6d49427
Rohit <rohit2000sharma06@gmail.com>
2026-08-06 11:57:55 +0530

    Create to-do list project structure

---
+++ 05_todo_list/README.md
@@ -0,0 +1,11 @@
+## Version 1
+
+### Features
+- Created project structure
+- Displayed menu
+- Accepted user menu choice
+
+### New Concepts Learned
+- Lists
+- Menu-driven programs
+- User input
---
+++ 05_todo_list/todo_list.py
@@ -0,0 +1,8 @@
+tasks = []
+
+print("========== TO-DO LIST ==========")
+print("1. View Tasks \n2. Add Task \n3. Remove Task \n4. Exit")
+print("===============================")
+
+choice = int(input("Enter your choice: "))
+print(f"You selected: {choice}")
