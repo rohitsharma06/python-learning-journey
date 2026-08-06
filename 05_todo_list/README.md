6897a9190dacf4372861d678449efb9b427fd3b6
Rohit <rohit2000sharma06@gmail.com>
2026-08-06 12:27:11 +0530

    Add task management and display

--- 05_todo_list/README.md
+++ 05_todo_list/README.md
@@ -9,3 +9,20 @@
 - Lists
 - Menu-driven programs
 - User input
+
+## Version 2
+
+### Features
+- Added tasks to the list
+- Display tasks
+- Display tasks with numbering
+- Added exit option
+- Continuous menu using `while True`
+
+### New Concepts Learned
+- `append()`
+- `for` loop
+- `range()`
+- `len()`
+- List indexing
+- `break`
--- 05_todo_list/todo_list.py
+++ 05_todo_list/todo_list.py
@@ -1,8 +1,26 @@
 tasks = []
+while True:
+    print("========== TO-DO LIST ==========")
+    print("1. View Tasks \n2. Add Task \n3. Remove Task \n4. Exit")
+    print("===============================")
 
-print("========== TO-DO LIST ==========")
-print("1. View Tasks \n2. Add Task \n3. Remove Task \n4. Exit")
-print("===============================")
+    choice = int(input("Enter your choice: "))
+    print(f"You selected: {choice}")
 
-choice = int(input("Enter your choice: "))
-print(f"You selected: {choice}")
+    if choice == 1:
+        for i in range(len(tasks)):
+            print(f"{i +1}. {tasks[i]}")
+
+    if choice == 2:
+        task = input("Enter your task: ")
+        tasks.append(task)
+        print("Task Added Successfully")
+
+    if choice == 3:
+        task_number = input("Enter task number to remove: ")
+        tasks.remove(task_number)
+        print("Task Removed Successfully")
+
+    if choice == 4:
+        print("Thank you for using To-Do List!")
+        break
