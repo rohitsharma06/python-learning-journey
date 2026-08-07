69fe3c76c0e5c4b1ec8347d98e99637f0fa8afbe
Rohit <rohit2000sharma06@gmail.com>
2026-08-07 21:21:26 +0530

    Latest Code

--- 06_contact_book/contact_book.py
+++ 06_contact_book/contact_book.py
@@ -1,6 +1,7 @@
 contacts = []
 
 while True:
+
     print("========== CONTACT BOOK ==========")
     print("1. View Contacts")
     print("2. Add Contact")
@@ -9,34 +10,51 @@ while True:
     print("5. Exit")
     print("==================================")
 
-    choice = int(input("Enter your choice: "))
+    try:
+        choice = int(input("Enter your choice: "))
+    except ValueError:
+        print("Please enter numbers only.")
+        continue
+
+    if choice < 1 or choice > 5:
+        print("Please enter a number between 1 and 5.")
+        continue
 
     if choice == 1:
 
         if len(contacts) == 0:
-            print("No contacts Available")
+            print("No contacts available.")
         else:
             for contact in contacts:
                 print("----------------------------")
                 print(f"Name  : {contact['name']}")
                 print(f"Phone : {contact['phone']}")
                 print(f"Email : {contact['email']}")
+                print("----------------------------")
 
-    if choice == 2:
+    elif choice == 2:
 
         name = input("Enter name: ")
         phone = input("Enter phone number: ")
         email = input("Enter email: ")
 
-        contact = {"name": name, "phone": phone, "email": email}
+        contact = {
+            "name": name,
+            "phone": phone,
+            "email": email
+        }
+
         contacts.append(contact)
 
         print("Contact added successfully.")
 
-    if choice == 3:
+    elif choice == 3:
+
         search_name = input("Enter contact name to search: ")
         found = False
+
         for contact in contacts:
+
             if contact["name"] == search_name:
                 print("\n---------- Contact Found ----------")
                 print(f"Name  : {contact['name']}")
@@ -46,12 +64,15 @@ while True:
                 found = True
 
         if found == False:
-            print("Contact not found")
+            print("Contact not found.")
 
-    if choice == 4:
+    elif choice == 4:
+
         delete_name = input("Enter contact name to delete: ")
         found = False
+
         for contact in contacts:
+
             if contact["name"] == delete_name:
                 contacts.remove(contact)
                 print("Contact deleted successfully.")
@@ -61,3 +82,7 @@ while True:
         if found == False:
             print("Contact not found.")
 
+    elif choice == 5:
+
+        print("Thank you for using Contact Book!")
+        break
