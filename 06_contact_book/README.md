03401b47575629ade8d75ed1d90ceac688d821f8
Rohit <rohit2000sharma06@gmail.com>
2026-08-07 09:52:33 +0530

    Create Contact Book project structure

---
+++ 06_contact_book/README.md
@@ -0,0 +1,12 @@
+# Contact Book
+
+## Version 1
+
+### Features
+- Project structure created
+- Contact Book menu added
+
+### New Concepts
+- Project planning
+- Menu-driven program
+
---
+++ 06_contact_book/contact_book.py
@@ -0,0 +1,14 @@
+contacts = []
+
+while True:
+    print("========== CONTACT BOOK ==========")
+    print("1. View Contacts")
+    print("2. Add Contact")
+    print("3. Search Contact")
+    print("4. Delete Contact")
+    print("5. Exit")
+    print("==================================")
+
+    choice = int(input("Enter your choice: "))
+
+
